03ac2c820cc7d2ea53bd005a6100344f764e5c61
Daniel O'Donnell <dan@dvoc.ca>
2016-05-20 14:49:10 -0400

    wyjetyjty

--- src/js/component/input/autocomplete.js
+++ src/js/component/input/autocomplete.js
@@ -21,6 +21,8 @@ export default Component.createClass({
 
         var minSearchLength = attrs.minSearchLength || 1;
 
+        console.log(this.state);
+
         els.push(Input({
             value : this.state.selectedLabel,
             onChange : function(e) {
@@ -47,8 +49,6 @@ export default Component.createClass({
                         if (attrs.service.onSelect) {
                             attrs.service.onSelect(e.target.id);
                         }
-                        console.log(e.target);
-                        console.log(e.target.innerHTML);
                         self.setState({selectedLabel:e.target.innerHTML});
                     }
                 }, this.state.results[i].label));
